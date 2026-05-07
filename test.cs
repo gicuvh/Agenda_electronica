821e0448eaecb8e776e835c8801c374e4fa0c4b7
gicuvoloh27@gmail.com <gicuvoloh27@gmail.com>
2026-05-07 08:30:29 +0300

    Initial commit

---
+++ test.cs
@@ -0,0 +1,10 @@
+using System; // Import the System namespace for basic functions
+
+public class HelloWorld 
+{
+    public static void Main(string[] args) 
+    {
+        // Print text to the console
+        Console.WriteLine("Hello, World!"); 
+    }
+}
